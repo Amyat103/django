64e918b409287c798e1f68099e5f28954f66baf2
Amyat103 <zhoudavid7312@gmail.com>
2025-09-16 11:22:56 -0400

    initliazed restaurant project

---
+++ restaurant/urls.py
@@ -0,0 +1,15 @@
+# File: restaurant/urls.py
+# Author: David Myat (amyat@bu.edu), 9/9/2025
+# Description: URL routing for restaurant project
+
+from django.conf import settings
+from django.urls import path
+
+from . import views
+
+# URL pattern specific to the restaurant app
+urlpatterns = [
+    path(r"main/", views.main, name="main_page"),
+    path(r"order/", views.order, name="order_page"),
+    path(r"confirmation/", views.confirmation, name="confirmation_page"),
+]
