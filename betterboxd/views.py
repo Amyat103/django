83ccf81f793f05d0a0933b7da8bd68b91bd67a05
Amyat103 <zhoudavid7312@gmail.com>
2025-12-02 11:28:09 -0500

    add base views and html to return template

--- betterboxd/views.py
+++ betterboxd/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+    """Function to respond to home request."""
+
+    template_name = "betterboxd/home.html"
+    return render(request, template_name)
+
+
+def profile(request):
+    """Function to respond to profile request."""
+
+    template_name = "betterboxd/profile.html"
+    return render(request, template_name)
+
+
+def movie_detail(request, movie_id):
+    """Function to respond to movie detail request."""
+
+    context = {
+        "movie_id": movie_id,
+    }
+
+    template_name = "betterboxd/movie_detail.html"
+    return render(request, template_name, context)
+
+
+def login(request):
+    """Function to respond to login request."""
+
+    template_name = "betterboxd/login.html"
+    return render(request, template_name)
